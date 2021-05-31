0e935ae55407951b382be54a91c9c4880d7b5c38
Dennis Andersson <dennis.andersson@gmail.com>
2021-05-31 03:15:19 +0200

    added siox

---
+++ Controller.cpp
@@ -0,0 +1,142 @@
+#include "Controller.h"
+#include <QString>
+#include <vector>
+#include <windows.h>
+
+Controller::Controller()
+{
+	bus = 0;
+}
+
+Controller::~Controller()
+{
+	if (bus >= 0)
+		ExitSioxBus(bus);
+}
+
+bool Controller::init(QString port)
+{
+	bus = InitSioxBus((char*)port.toStdString().c_str(), 4800);
+
+	return (bus >= 0);
+}
+
+void Controller::test()
+{
+	//// Activate 2 of seven outputs
+	//std::vector<int> message{ 0xc1, 0x03 };
+	//std::vector<int> answer;
+
+	//answer = send(message);
+
+	//if (answer.size() == 2) {
+	//	int passive = answer[0];
+	//	int active = answer[1];
+	//}
+}
+
+std::vector<int> Controller::showTargets()
+{
+	return { 0 };
+}
+
+std::vector<int> Controller::hideTargets()
+{
+	return { 0 };
+	//std::vector<int> message{ 0xc1, 0x02 };
+
+	//return send(message);
+}
+
+std::vector<int> Controller::activate(int port)
+{
+	int n = 1 << (port - 1);
+
+	std::vector<int> message{ 0xc1, n };
+
+	return send(message);
+}
+
+std::vector<int> Controller::deactivate(int port)
+{
+	int n = 0;
+
+	std::vector<int> message{ 0xc1, n };
+
+	return send(message);
+}
+
+std::vector<int> Controller::activateBoth()
+{
+	std::vector<int> message{ 0xc1, 0x03 };
+
+	return send(message);
+}
+
+std::vector<int> Controller::deactivateBoth()
+{
+	std::vector<int> message{ 0xc1, 0x00 };
+
+	return send(message);
+}
+
+std::vector<int> Controller::send(std::vector<int> message)
+{
+	if (bus < 0) {
+		//logger.log("No valid bus handle");
+		return { 0 };
+	}
+
+	std::vector<int> response;
+
+	//for (char ch : message)
+	//	SendChar(bus, ch);
+
+	//while (!CharReceived(bus))
+	//	Sleep(1);
+
+	//while (CharReceived(bus))
+	//	response.push_back(GetChar(bus, 0));
+
+	response.push_back(0x12);
+	response.push_back(0x34);
+
+	return response;
+}
+
+QString Controller::getErrorMessage()
+{
+	const int SIZE{ 120 };
+	char message[SIZE];		// Needs to have space for at least 80 chars according to documentation
+
+	InitSioxBusErrorMsg(bus, message);
+
+	return QString(message);
+}
+
+QString Controller::result()
+{
+	std::vector<QString> errorMessages {
+		"",
+		"The module is not responding (silent)",
+		"The SIOX bus can not be accessed (e.g. network error)",
+		"Invalid bus handle",
+		"Function not supported in this mode",
+		"Bus short circuit",
+		"Communication error",
+		"Possibly double addressed modules"
+	};
+
+	QString result;
+	int errorCode;
+
+	errorCode = SioxResult(bus);
+
+	if ((errorCode > 0) && (errorCode < 8))
+		result = "Error: " + errorMessages[errorCode];
+	else
+		result = QString::asprintf("Result: %d", errorCode);
+
+	return result;
+}
+
---
+++ Controller.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Siox30.h>
+#include <QString>
+#include <string>
+#include <vector>
+
+class Controller
+{
+	int portTable[12]{ 3, 4, 7, 8, 11, 12, 15, 16, 19, 20, 23, 24 };
+	int bus{ 0 };
+
+	QString hexstr(int n);
+
+public:
+	Controller();
+	~Controller();
+
+	bool init(QString port);
+
+	QString getErrorMessage();
+	QString result();
+
+	std::vector<int> activate(int port);
+	std::vector<int> deactivate(int port);
+	std::vector<int> activateBoth();
+	std::vector<int> deactivateBoth();
+	std::vector<int> showTargets();
+	std::vector<int> hideTargets();
+	std::vector<int> send(std::vector<int> message);
+	void log(bool sent, std::vector<int> message);
+	void log(QString message);
+
+	void test();	// Test hardware function
+};
+
